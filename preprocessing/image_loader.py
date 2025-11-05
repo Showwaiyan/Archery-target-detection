1b5863ddcbed312071f51991f7f6cf713cd2b793
Show Wai Yan <showwaiyan555@gmail.com>
2025-11-05 09:04:12 +0800

    Add image show function

--- preprocessing/image_loader.py
+++ preprocessing/image_loader.py
@@ -1,6 +1,14 @@
 # Function to load an image from file path
 # Function to display image in a window
-from cv2 import imread, IMREAD_COLOR
+from cv2 import (
+    imread,
+    imshow,
+    namedWindow,
+    waitkey,
+    destroyAllWindows,
+    IMREAD_COLOR,
+    WINDOW_AUTOSIZE,
+)
 
 
 def load_image(img_name, color_type=IMREAD_COLOR):
@@ -11,3 +19,13 @@ def load_image(img_name, color_type=IMREAD_COLOR):
         raise FileNotFoundError(f"File Note Found: {path+img_name}")
 
     return img
+
+
+def show_image(image, name, size=WINDOW_AUTOSIZE):
+    if image is None:
+        raise ValueError("Invalid Image: image value is None")
+
+    namedWindow(name, size)
+    imshow(name, image)
+    waitkey(0)
+    destroyAllWindows()
