33a2fe4e1677b13bf88bebf86e4b9a43b2a5a1a1
Show Wai Yan <showwaiyan555@gmail.com>
2025-11-06 22:53:29 +0800

    Add cnotrast validation function

--- preprocessing/image_validator.py
+++ preprocessing/image_validator.py
@@ -9,13 +9,26 @@ def validate_blur(image, threshold=100.0):
 
 
 def validate_brightness(image, light_threshold=90.0, dark_threshold=15.0):
-    if len(image.shape) == 2:
-        raise ValueError("Only grayscale image")
+    if len(image.shape) != 2:
+        raise ValueError("Only grayscale image is acceptable")
     mean_brightness = image.mean() * 100 / 255  # change to percentage
 
     if mean_brightness < dark_threshold:
         return -1  # -1 -> dark
     if mean_brightness > light_threshold:
         return 1  # 1 -> too bright
+
+    return 0  # normal
+
+
+def validate_contrast(image, low_threshold=20.0, high_threshold=80):
+    if len(image.shape) != 2:
+        raise ValueError("Only grayscale image is acceptable")
+    contrast = image.std()
+
+    if contrast < low_threshold:
+        return -1  # -1 -> low
+    if contrast > high_threshold:
+        return 1  # 1 -> high
 
     return 0  # normal
