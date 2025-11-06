7e0178cbbf30ff74761a1c6bc60b008ba4af8f2e
Show Wai Yan <showwaiyan555@gmail.com>
2025-11-06 22:25:39 +0800

    Add brightness validator function

--- preprocessing/image_validator.py
+++ preprocessing/image_validator.py
@@ -6,3 +6,16 @@ def detect_blur(image, threshold=100.0):
     laplacian = Laplacian(grayscale_image, CV_64F)
     variance = laplacian.var()
     return variance < threshold, variance
+
+
+def validate_brightness(image, light_threshold=90.0, dark_threshold=15.0):
+    if len(image.shape) == 2:
+        raise ValueError("Only grayscale image")
+    mean_brightness = image.mean() * 100 / 255  # change to percentage
+
+    if mean_brightness < dark_threshold:
+        return -1  # -1 -> dark
+    if mean_brightness > light_threshold:
+        return 1  # 1 -> too bright
+
+    return 0  # normal
