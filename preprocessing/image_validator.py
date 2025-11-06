cbb4ebe16f09e1872a3316ff47c07f970cf0d7aa
Show Wai Yan <showwaiyan555@gmail.com>
2025-11-06 22:28:16 +0800

    Rename detect_blur function to validate_blur

--- preprocessing/image_validator.py
+++ preprocessing/image_validator.py
@@ -1,7 +1,7 @@
 from cv2 import cvtColor, Laplacian, COLOR_BGR2GRAY, CV_64F
 
 
-def detect_blur(image, threshold=100.0):
+def validate_blur(image, threshold=100.0):
     grayscale_image = cvtColor(image, COLOR_BGR2GRAY)
     laplacian = Laplacian(grayscale_image, CV_64F)
     variance = laplacian.var()
