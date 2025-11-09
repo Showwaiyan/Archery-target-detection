9d67c73dd3043d47c220c492764a5e18cab25079
Show Wai Yan <showwaiyan555@gmail.com>
2025-11-09 16:17:24 +0800

    Move canny edge detection to morphological processing file

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,7 +1,7 @@
-# blur_detector/__init__.py
 from .image_validator import validate_blur, validate_brightness, validate_contrast
 from .image_loader import load_image, show_image
 from .image_transformer import resize_image, change_color_channel, crop_image
+from .morphological_processor import morphologic_edge_detection, auto_canny
 
 __all__ = [
     "validate_blur",
@@ -12,4 +12,6 @@ __all__ = [
     "resize_image",
     "change_color_channel",
     "crop_image",
+    "morphologic_edge_detection",
+    "auto_canny",
 ]
--- preprocessing/morphological_processor.py
+++ preprocessing/morphological_processor.py
@@ -2,30 +2,31 @@ import cv2
 import numpy as np
 
 
-def enhance_edges_for_detection(image):
-    """
-    Prepare an image for edge/circle detection using:
-    - grayscale conversion
-    - morphological gradient
-    - contrast enhancement
+def auto_canny(img, k=1.0):
+    sigma = img.std()  # Finding low/high contrast
+    mean = img.mean()  # Finsing dark/bright image
+
+    # thresholds from contrast
+    lower = 0.66 * sigma
+    upper = 1.33 * sigma
+
+    # brightness compensation
+    brightness_factor = np.clip(mean / 128, 0.5, 1.5)
+
+    lower = int(max(0, lower * brightness_factor * k))
+    upper = int(min(255, upper * brightness_factor * k))
+
+    # img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.medianBlur(img, 3)
+    return cv2.Canny(img, lower, upper), upper, lower
 
 
-    Args:
-    image: Input BGR or grayscale image
-
-
-    Returns:
-    preprocessed: Image after gradient + contrast enhancement
-    """
-    # TODO: Convert to grayscale if needed
-    # Example placeholder:
+def morphologic_edge_detection(image):
     if len(image.shape) != 2:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
-    # TODO: Apply morphological gradient
-    # Example placeholder:
     gradient = cv2.morphologyEx(gray, cv2.MORPH_GRADIENT, kernel)
 
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
