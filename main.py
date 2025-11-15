bea3c320e01a7b392229123674f4f2c3c4d69f6c
Show Wai Yan <showwaiyan555@gmail.com>
2025-11-15 10:21:24 +0800

    Add detection route to app

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+import routes
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+app.include_router(
+    routes.score_detection_router, prefix=routes.score_detection_route_prefix
+)
