b687e9ac409022386aa9d1a761eddd9936bcf327
Show Wai Yan <showwaiyan555@gmail.com>
2025-11-17 15:26:52 +0800

    Define arrows scores insertion for each ends

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,4 +1,4 @@
-from .score_detection_routes import router as score_detection_router
+from .score_detection_routes import router as score_detection_router, post_ends_scores
 from .score_detection_routes import router_prefix as score_detection_route_prefix
 
-__all__ = ["score_detection_router", "score_detection_route_prefix"]
+__all__ = ["score_detection_router", "post_ends_scores", "score_detection_route_prefix"]
--- routes/score_detection_routes.py
+++ routes/score_detection_routes.py
@@ -1,9 +1,26 @@
 from fastapi import APIRouter, UploadFile
-from controllers.score_detection_controller import detect_target_file
+from pydantic import BaseModel
+from typing import List, Any
+from controllers.score_detection_controller import detect_target_file, update_ends_scores
+
+
+class ArrowStagingPayload(BaseModel):
+    roundID: int
+    participationID: int
+    distance: int
+    target: int
+    endOrder: int
+    arrows: List[Any]
+
 
 tag = "scores-detaction"
 router_prefix = "arrowStaging"
 router = APIRouter(tags=[tag])
+
+
+@router.post("/{archer_id}/detect", tag=[tag])
+async def post_ends_scores(archer_id: int, payload: ArrowStagingPayload):
+    return await update_ends_scores(archer_id, payload)
 
 
 @router.post("/{range_id}/detect", tags=[tag])
