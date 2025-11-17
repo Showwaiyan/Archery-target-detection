cc7a13535de2b3747038a3a4761a255a91cc05be
Show Wai Yan <showwaiyan555@gmail.com>
2025-11-17 12:42:07 +0800

    Fix db connection, query async, db_name, and install cryptography for db password initialize

--- database.py
+++ database.py
@@ -13,19 +13,22 @@ async def init_pool():
         host=getenv("DB_HOST"),
         user=getenv("DB_USER"),
         password=getenv("DB_PASSWORD"),
-        db=getenv("DB_NAMEA"),
+        db=getenv("DB_NAME"),
         autocommit=True,
         minsize=1,
         maxsize=5,
     )
 
 
-async def execute_query(sql: str):
+async def execute_query(sql: str, params=None):
     global pool
+    if pool is None:
+        raise RuntimeError("Database pool is not initialized")
     result = None
+
     async with pool.acquire() as conn:
-        async with conn.cursor() as cursor:
-            await cursor.execute(sql)
-            result = await cursor.fetchall()
+        async with conn.cursor(aiomysql.DictCursor) as cursor:
+            await cursor.execute(sql, params)
+            return await cursor.fetchall()
 
     return result
--- repositories/range_repository.py
+++ repositories/range_repository.py
@@ -2,11 +2,10 @@ from database import execute_query
 
 
 async def get_total_arrows_per_end(range_id: int):
-    sql = f"SELECT rangeTotalArrowsPerEnd FROM range WHERE rangeID = {
-        range_id}"
-    rows = await execute_query(sql)
+    sql = f"SELECT rangeTotalArrowsPerEnd FROM `range` WHERE rangeID = %s"
+    rows = await execute_query(sql, (range_id,))
     if len(rows) == 0:
         return None
 
-    result = rows[0]
+    result = rows[0]["rangeTotalArrowsPerEnd"]
     return result
